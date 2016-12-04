081f82178f5d8c8b9714208772735611b5364c08
Li Jian <haiwuxing@gmail.com>
2016-12-05 06:17:14 +0800

    模拟点击链接后显示某一篇博文

---
+++ README.md
@@ -0,0 +1,2 @@
+试图尝试自己写一个博客。
+使用Visutal Studio 2015 创建的 Python Flask Blank Project。
--- routes.py
+++ routes.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template;
 from app import app;
 from models import *;
 
+posts = [];
+post1 = Post(id=1,title="标题1", text="content1", created_at="2015-01-01");
+post2 = Post(id=2,title="标题2", text="content2", created_at="2015-01-02");
+posts.append(post1);
+posts.append(post2);
+
 @app.route('/')
-def hello():
-    """Renders a sample page."""
-    posts = [];
-    post1 = Post(id=1,title="标题1", text="content1", created_at="2015-01-01");
-    post2 = Post(id=2,title="标题2", text="content2", created_at="2015-01-02");
-    posts.append(post1);
-    posts.append(post2);
+def index():
+    """首页"""
     return render_template('index.html', posts = posts);
+
+@app.route('/post/<int:post_id>', methods=['GET'] )
+def show_post(post_id):
+    """显示一篇博文"""
+    # show the post with the given id, the id is an integer
+    post = posts[post_id-1];
+    return render_template('post.html', post = post);
