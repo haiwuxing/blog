cb8c95b8ba94d43eaf12da7a716f1ecb0945295b
Li Jian <haiwuxing@gmail.com>
2016-12-11 06:50:19 +0800

    可以在命令行中通过create_all创建表和数据库。

--- app.py
+++ app.py
@@ -17,6 +17,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True;
 
 db = SQLAlchemy(app);
 
+class Post(db.Model):
+    __tablename__='posts';
+
+    id = db.Column(db.Integer, primary_key=True);
+    title = db.Column(db.String);
+    body = db.Column(db.Text);
+    #created_at =  db.Column(db.DateTime, default=db.func.now());
+
+    def __init__(self, title, body):
+        self.title = title;
+        self.body = body;
+    
+    def __repr__(self):
+        return '<Post> %r>' % self.title;
+
 # import all of our routes from routes.py
 #from views import *;
 
