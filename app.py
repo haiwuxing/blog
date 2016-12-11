ccc34dfa4a0cbf540f4ddf8735d3531c50e140c8
Li Jian <lijian@hantek.com>
2016-12-11 09:16:31 +0800

    将Post 类从 app.py 中挪到 models.py 中

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import os;
 from flask import Flask;
-from flask_sqlalchemy import SQLAlchemy;
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -12,28 +10,12 @@ app.config.update(dict(
     PASSWORD='admin',
 ));
 
+# TODO: 怎样通过绝对路径////指定。
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db';
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True;
 
-db = SQLAlchemy(app);
-
-class Post(db.Model):
-    __tablename__='posts';
-
-    id = db.Column(db.Integer, primary_key=True);
-    title = db.Column(db.String);
-    body = db.Column(db.Text);
-    created_at =  db.Column(db.DateTime, default=db.func.now());
-
-    def __init__(self, title, body):
-        self.title = title;
-        self.body = body;
-    
-    def __repr__(self):
-        return '<Post> %r>' % self.title;
-
 # import all of our routes from routes.py
-#from views import *;
+from views import *;
 
 if __name__ == '__main__':
     app.run(debug=True);
--- models.py
+++ models.py
@@ -1,26 +1,10 @@
 '''
 Models for post
 '''
+from app import app;
+from flask_sqlalchemy import SQLAlchemy;
 
-import time;
-from datetime import date;
-
-
-class Post1(object):
-    """代表一篇博文"""
-    def __init__(self, id=None, title=None, text=None):
-        self.title = title;
-        self.text = text;
-        self.created_at = time.time();
-
-    @property
-    def created_at(self):
-        dt = date.fromtimestamp(self.__created_at);
-        return '%s年%s月%s日' % (dt.year, dt.month, dt.day);
-
-    @created_at.setter
-    def created_at(self, created_at):
-        self.__created_at = created_at;
+db = SQLAlchemy(app);
 
 class Post(db.Model):
     __tablename__='posts';
@@ -28,7 +12,8 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True);
     title = db.Column(db.String);
     body = db.Column(db.Text);
-    #created_at =  db.Column(db.DateTime, default=db.func.now());
+    # TODO: func.now() 时区问题。
+    created_at =  db.Column(db.DateTime, default=db.func.now());
 
     def __init__(self, title, body):
         self.title = title;
@@ -39,6 +24,5 @@ class Post(db.Model):
 
 # 一些测试代码
 if __name__ == '__main__':
-    db.create_all();
-    #post = Post('aaa','111');
-    #print(post.created_at);
+    post = Post('aaa','111');
+    print(post.created_at);
