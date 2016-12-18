5c1fb7e7ae105c6a142597271748cf7a74408854
Li Jian <lijian@hantek.com>
2016-12-18 11:29:56 +0800

    可以根据 id  显示不同的文章。

--- client/js/main.js
+++ client/js/main.js
@@ -6,10 +6,10 @@ app.config(function ($routeProvider) {
         templateUrl: "post_list.html",
         controller: 'PostListController'
     })
-    .when("/show_post", {
+    .when("/show_post/:post_id", {
         templateUrl: "show_post.html",
         controller: "ShowPostController"
-     })
+    })
     .otherwise({
         redirectTo: '/'
     });
@@ -24,10 +24,11 @@ app.controller("PostListController", function ($scope, $http) {
     });
 });
 
-app.controller("ShowPostController", function ($scope, $http) {
-    $http.get("http://localhost:5000/api/v1.0/posts/1")
+app.controller("ShowPostController", function ($scope, $http, $routeParams) {
+    post_id = $routeParams.post_id;
+    rest_url = "http://localhost:5000/api/v1.0/posts/" + post_id;
+    $http.get(rest_url)
     .then(function (response) {
-        console.log("Success");
         $scope.post = response.data.post;
         console.log(response);
     });
